fa215d20ff5e874895da96a8c214aab97207efcd
identificador3ds-design <identificador3ds@gmail.com>
2026-03-04 11:59:49 -0300

    Update app.py

--- Metodo/app.py
+++ Metodo/app.py
@@ -57,6 +57,8 @@ def analisar():
     except Exception as e:
         return jsonify({"status": "erro", "mensagem": str(e)}), 500
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
-    app.run(debug=True, port=5000)
